c65b6b3e3e097cea6dd0edd0ffe841ffa49fe1bf
Jokyo_sp_rapid <26354730+jokyo-sp-rapid@users.noreply.github.com>
2020-03-27 19:51:51 +0900

    Update main.py

--- main.py
+++ main.py
@@ -22,3 +22,22 @@ def three_quarters(img,bg_color):
 			x_pos+=1
 	
 	return new_img
+
+def three_halves(img):
+	
+	new_img = PIL.Image.new("RGBA", ( img.size[0] / 2 * 3 , img.size[1] / 2 * 3 ) , (0,0,0,255) )
+	
+	for x in range( img.size[0]/pitch ):
+		for y in range( img.size[1]/pitch ):
+			x_pos = x*3//2
+			y_pos = y*3//2
+			temp = img.crop( ( x*pitch , y*pitch , (x+1)*pitch , (y+1)*pitch ) )
+			new_img.paste(temp,(x_pos*pitch,y_pos*pitch),temp)
+			if x%2 == 1:
+				new_img.paste(temp,((x_pos+1)*pitch,y_pos*pitch),temp)
+				if y%2 == 1:
+					new_img.paste(temp,((x_pos+1)*pitch,(y_pos+1)*pitch),temp)
+			if y%2 == 1:
+				new_img.paste(temp,(x_pos*pitch,(y_pos+1)*pitch),temp)
+	
+	return new_img
